2708602cbeebd9d74130faac9388de43d917fe03
Fernando Senna <fmmsenna@gmail.com>
2024-08-19 17:37:31 -0300

    NewContact Component "vanilla" ✅

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import NewContact from "./NewContact";
 
 function ContactList() {
+  const [contact, setContact] = useState({
+    name: "",
+    number: "",
+    url: "",
+  });
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    console.log("changed here");
+    setContact((prevContact) => ({
+      ...prevContact,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!contact.name || !contact.number || !contact.url) {
+      console.log("Fill in the form");
+    } else {
+      console.log("Created");
+    }
+  }
+
   return (
     <div>
       <h2>ContactList</h2>
-      <NewContact />
+      <button>New +</button>
+      <NewContact
+        contact={contact}
+        handleSubmit={handleSubmit}
+        handleInputChange={handleInputChange}
+      />
     </div>
   );
 }
--- src/components/NewContact.jsx
+++ src/components/NewContact.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 
-function NewContact() {
-  return <div>NewContact</div>;
+function NewContact(props) {
+  return (
+    <form onSubmit={(event) => props.handleSubmit(event)}>
+      <h1>New Contact</h1>
+      <label htmlFor="name">Name</label>
+      <br />
+      <input
+        id="name"
+        type="text"
+        name="name"
+        value={props.contact.name}
+        onChange={(event) => props.handleInputChange(event)}
+      />
+      <br />
+      <label htmlFor="phone-input">Phone number</label>
+      <br />
+      <input
+        placeholder="(  )    -    "
+        name="number"
+        value={props.contact.number}
+        onChange={(event) => props.handleInputChange(event)}
+      />
+      <br />
+      <label htmlFor="url">Profile picture</label>
+      <br />
+      <input
+        id="url"
+        type="text"
+        name="url"
+        value={props.contact.url}
+        onChange={(event) => props.handleInputChange(event)}
+      />
+      <br />
+      <button>Submit</button>
+    </form>
+  );
 }
 
 export default NewContact;
