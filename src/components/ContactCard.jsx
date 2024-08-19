c9f19ace7a8772458833eed5d9a283b006c0f7af
Fernando Senna <fmmsenna@gmail.com>
2024-08-19 18:49:08 -0300

    contact card component structure

---
+++ src/components/ContactCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ContactCard({ name, number }) {
+  return (
+    <div>
+      <img src="" />
+      <p>{name}</p>
+      <small>{number}</small>
+      <button>X</button>
+    </div>
+  );
+}
+
+export default ContactCard;
