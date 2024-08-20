4a44ed26a1f04f2fc7138ffc9f3d318673a9d435
Fernando Senna <fmmsenna@gmail.com>
2024-08-20 15:25:21 -0300

    adjusted img size

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ContactCard({ name, number, url }) {
   return (
     <div>
-      <img src={url} />
+      <img src={url} className="card--img" />
       <p>{name}</p>
       <small>{number}</small>
       <button>X</button>
