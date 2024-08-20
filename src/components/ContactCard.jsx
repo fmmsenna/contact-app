19f812f0abc6a605671d987fb80c89ca00c5efc0
Fernando Senna <fmmsenna@gmail.com>
2024-08-20 18:02:27 -0300

    remove feature done

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import supabase from "../supabase";
 
 function ContactCard({ contact }) {
+  async function handleDelete() {
+    const { data, error } = await supabase
+      .from("contacts")
+      .delete()
+      .eq("user_id", contact.user_id)
+      .select();
+    if (error) {
+      console.log(error);
+    }
+    if (data) {
+      console.log(data);
+    }
+  }
+
   return (
     <div>
       <img src={contact.profile_url} className="card--img" />
       <p>{contact.name}</p>
       <small>{contact.phone_number}</small>
-      <button>X</button>
+      <button onClick={handleDelete}>X</button>
     </div>
   );
 }
