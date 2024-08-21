4f552cb3387cfd05905658dec7d03ed315956e2f
Fernando Senna <fmmsenna@gmail.com>
2024-08-21 18:02:25 -0300

    sign-in complete

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,48 @@
-import React, { useEffect } from "react";
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
+import React, { useEffect, useState } from "react";
 import supabase from "../supabase";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
-  console.log(supabase.auth.getSession());
+  const [user, setUser] = useState({ email: "", password: "" });
+  const [formError, setFormError] = useState(null);
 
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      console.log(session);
+  const navigate = useNavigate();
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setFormError("");
+
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
+  }
+
+  async function signInWithEmail(e) {
+    e.preventDefault();
+
+    if (!user.password || user.password.length < 6) {
+      setFormError("Senha incorreta");
+      return;
+    } else if (!user.email) {
+      setFormError("Preencha o seu email cadastrado");
+      return;
+    }
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: user.email,
+      password: user.password,
     });
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      console.log(session);
-    });
-
-    return () => subscription.unsubscribe();
-  }, []);
+    if (error) {
+      setFormError("Email e/ou senha incorretos.");
+      return;
+    }
+    if (data) {
+      console.log("Done!");
+      //   navigate("/contact-list"); - PRECISO ROUTEAR PARA O ID CORRETO DA PESSOA
+    }
+  }
 
   return (
     <div>
@@ -28,17 +51,32 @@ function Login() {
       </small>
 
       <h1>Sign-in to Contacts App ✅</h1>
-      <label htmlFor="email">Your email</label>
-      <br />
-      <input type="text" id="email" />
-      <br />
-      <label htmlFor="password">Password</label>
-      <br />
-      <input type="text" id="password" />
-      <br />
-      <button>Login</button>
+      <form onSubmit={signInWithEmail}>
+        <label htmlFor="email">Your email</label>
+        <br />
+        <input
+          type="text"
+          id="email"
+          name="email"
+          onChange={(event) => handleChange(event)}
+          value={user.email}
+        />
+        <br />
+        <label htmlFor="password">Password</label>
+        <br />
+        <input
+          type="text"
+          id="password"
+          name="password"
+          onChange={(event) => handleChange(event)}
+          value={user.password}
+        />
+        <br />
+        <button>Login</button>
+      </form>
       <br />
       <button>Login with magic link ✨</button>
+      <p>{formError}</p>
     </div>
   );
 }
