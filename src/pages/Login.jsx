70c6ecb4123fab31c68de12a03b8694d3d3045eb
Fernando Senna <fmmsenna@gmail.com>
2024-08-21 17:06:13 -0300

    added layout to sign-in/out pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { Link } from "react-router-dom";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <small>
+        <Link to="/sign-up">Sign-up</Link>
+      </small>
+
+      <h1>Sign-in to Contacts App ✅</h1>
+      <label htmlFor="email">Your email</label>
+      <br />
+      <input type="text" id="email" />
+      <br />
+      <label htmlFor="password">Password</label>
+      <br />
+      <input type="text" id="password" />
+      <br />
+      <button>Login</button>
+      <br />
+      <button>Login with magic link ✨</button>
+    </div>
+  );
 }
 
 export default Login;
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <h1>Create an account</h1>
+      <label htmlFor="email">Your email</label>
+      <br />
+      <input type="text" id="email" />
+      <br />
+      <label htmlFor="password">Set a strong password</label>
+      <br />
+      <input type="text" id="password" />
+      <br />
+      <button>Sign-up</button>
+    </div>
+  );
 }
 
 export default SignUp;
