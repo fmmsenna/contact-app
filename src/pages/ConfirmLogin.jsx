9438b462bf163914ccefdef535d94b62862e2019
Fernando Senna <fmmsenna@gmail.com>
2024-08-26 18:20:38 -0300

    confirmation page css done

--- src/pages/ConfirmLogin.jsx
+++ src/pages/ConfirmLogin.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 function ConfirmLogin() {
-  return <div>✅Check o seu email para ter acesso</div>;
+  return (
+    <div className="container">
+      <div className="confirmation">
+        <img
+          src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExMWw0bjkyNXZsNHlneGRhZ3V0d2Q0a21hazF2aTJuMTE0eHp0dTRrcCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/0Ul3g0DvDMdsFAn7NN/giphy.gif"
+          className="confirmation--img"
+        />
+        <h1 className="confirmation--h1">Magic link is in your inbox</h1>
+        <p className="confirmation--p">
+          We also hate passwords. So we just emailed you with a confirmation
+          link. Click it to sign-in! ✅
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default ConfirmLogin;
